03c01e39dc93e63fff355ab80bb6fd86f7655382
Andy Hotzler <ahotzler@gmail.com>
2021-03-11 13:59:45 -0600

    movies.controller completed for now

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,35 +1,49 @@
 const MoviesService = require("./movies.service");
+const treeize = require('../utils/treeize'); // import treeize package
 
 
-function movieExists(req, res, next) {
-    const error = { status: 404, message: `Movie ID cannot be found.`}
+// VALIDATION MIDDLEWARE; EXPORT AT BOTTOM
+async function movieExists(req, res, next) {
+    const error = { status: 404, message: `Movie ID cannot be found.`};
     
     const { movieId } = req.params;
-    if(!movieId) return next(error);
+    if (!movieId) return next(error);
 
-    MoviesService.getMovieById(movieId).then((movie) => {
-        if(!movie) return next(error);
-        res.locals.movie = movie;
-        next();
-    });
+    const movie = await MoviesService.read(movieId);
+    if (!movie) return next(error);
+    
+    res.locals.movie = movie;
+    next();
+    
 };
 
 
 // Retrieves a list of all movies in "movies" table
-function list(req, res, next) {
-    MoviesService.getAllMovies()
-        .then((movies) => {
-            res.status(200).json({ data: movies})
-        })
+async function list(req, res, next) {
+    const { is_showing } = req.query;
+
+    let list = await MoviesService.list();
+    list = treeize(list);
+
+    let showingList = await MoviesService.isShowing();
+    showingList = treeize(showingList);
+
+    if (is_showing !== true) {
+        res.json({ data: list });
+    } else {
+        res.json({ data: showingList });
+    };
 };
+
 
 // Retrieves movie associated with id
 function read(req, res, next) {
     const { movie } = res.locals;
     res.json({ data: movie })
-}
+};
 
 module.exports = {
     list,
     read: [movieExists, read],
-}
+    movieExists,
+};
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -2,7 +2,8 @@ const knex = require("../db/connection");
 
 const getAllMovies = () => knex("movies").select("*");
 
-const getMovieById = (movieId) => knex("movies")
+const getMovieById = (movieId) => 
+    knex("movies")
     .select("*")
     .where({ id: movieId })
     .first();
